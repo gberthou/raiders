6bdac5636d307a74cd8d6ebe9834d88e3664f3e2
gberthou <gautier.berthou@insa-lyon.fr>
2017-01-21 11:05:39 +0100

    Added clock support

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
 
     window = sf.RenderWindow(sf.VideoMode(600, 600), "Raiders")
     window.vertical_synchronization = True
+    window.framerate_limit = 60
 
     entityManager = ecs.EntityManager()
     eventManager  = ecs.EventManager()
@@ -21,14 +22,19 @@ if __name__ == "__main__":
     pelo.addComponent(components.Fighter(0, 0, 0, 0))
     pelo.addComponent(components.DrawableFighter(sf.RectangleShape((TILE_SIZE, TILE_SIZE))))
 
+    clock = sf.Clock()
+
     while window.is_open:
         for event in window.events:
             if type(event) is sf.CloseEvent:
                 window.close()
 
-        pelo.component(components.Position).x += 1
+        dt = clock.elapsed_time.seconds
+        clock.restart()
+
+        pelo.component(components.Position).x += 100*dt # 100px/s
 
         window.clear(sf.Color(0, 255, 0))
-        app.updateAll(0)
+        app.updateAll(dt)
         window.display()
 
