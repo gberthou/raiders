caf2c4004f87c95445cb1f3fa2fb9c58527a4738
Douglas RAILLARD <public.douglas.raillard@gmail.com>
2017-01-27 21:35:34 +0000

    Add standard deviation to dijkstraTest.py

--- dijkstraTest.py
+++ dijkstraTest.py
@@ -1,5 +1,6 @@
 import dijkstra
 import time
+import statistics
 
 area  = (0, 0, 70, 70)
 start = (10, 10)
@@ -7,10 +8,11 @@ goal  = (40, 60)
 
 t = []
 N = 10
-for i in range(N): 
+for i in range(N):
     a = time.time()
     tmp = dijkstra.searchPath(area, start, goal)
     t.append(time.time() - a)
 
-print(sum(t) / N)
+print(statistics.mean(t))
+print(statistics.stdev(t))
 
