512aca025fce1195479874f003fff27233d65460
Douglas RAILLARD <public.douglas.raillard@gmail.com>
2017-01-27 22:13:03 +0000

    Optimize A*

--- dijkstra.pyx
+++ dijkstra.pyx
@@ -1,12 +1,17 @@
 # All coordinates are in tile space
+cimport libc.limits
+from libc.stdint cimport uint32_t, int64_t
 
 def neighborsOf(pos):
     cdef int x, y
     x, y = pos
-    return [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
+    return ((x+1, y), (x-1, y), (x, y+1), (x, y-1))
 
-cdef int distance(p0, p1):
-    return abs(p0[0]-p1[0]) + abs(p0[1]-p1[1])
+cdef unsigned int distance(p0, p1):
+    cdef int64_t x0, x1, y0, y1
+    x0,y0 = p0
+    x1, y1 = p1
+    return abs(x0 - x1) + abs(y0 - y1)
 
 # x1 > x0
 # y1 > y0
@@ -16,13 +21,18 @@ def dijkstra(area, start, goal):
     x0, y0, x1, y1 = area
     startX, startY = start
     goalX, goalY   = goal
-    dist = {(startX, startY) : 0}
     nQ = set()
     prev = dict()
+    
+    dist = {(startX, startY) : 0}
 
-    cdef int i
+    cdef int i, j, d
     for i in range((x1-x0+1) * (y1-y0+1)):
-        d, minDistPoint = min([(j, x) for x, j in dist.items() if not x in nQ])
+        d = libc.limits.INT_MAX
+        for x, j in dist.items():
+            if j < d and (x not in nQ):
+                d = j
+                minDistPoint = x
 
         if minDistPoint == goal: # Mathematical proof of correctness?
             break
@@ -34,7 +44,7 @@ def dijkstra(area, start, goal):
             if neighbor in nQ:
                 continue
             alt = d + 1 + distance(neighbor, goal) # A* like
-            if not neighbor in dist.keys() or alt < dist[neighbor]:
+            if neighbor not in dist.keys() or alt < dist[neighbor]:
                 dist[neighbor] = alt
                 prev[neighbor] = minDistPoint
     return prev
@@ -44,7 +54,7 @@ def searchPath(area, start, goal):
         return [start]
 
     prev = dijkstra(area, start, goal)
-    if not goal in prev.keys(): # No path
+    if goal not in prev.keys(): # No path
         return None
 
     ret = [goal]
--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
 
-setup(ext_modules = cythonize("dijkstra.pyx"))
+extensions = [
+    Extension('dijkstra',
+        ['dijkstra.pyx'],
+        extra_compile_args=["-g3"],
+        extra_link_args=["-g3"]
+    )
+]
+
+setup(ext_modules=cythonize(extensions, gdb_debug=True))
