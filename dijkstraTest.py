b3eb0cf5989915813e4bdb11c4c04518c6825484
Douglas RAILLARD <public.douglas.raillard@gmail.com>
2017-01-31 02:45:42 +0000

    Add NetPBM P3 output to dijkstraTest.py
    It can be displayed by piping this output to "convert /dev/stdin path.py"
    If the NetPBM output is not desired, one can discard the standard output
    and just display the stderr stream.

--- dijkstraTest.py
+++ dijkstraTest.py
@@ -1,6 +1,8 @@
 import dijkstra
 import time
 import statistics
+import collections
+import sys
 
 area  = (0, 0, 70, 70)
 start = (10, 10)
@@ -13,6 +15,23 @@ for i in range(N):
     tmp = dijkstra.searchPath(area, start, goal)
     t.append(time.time() - a)
 
-print(statistics.mean(t))
-print(statistics.stdev(t))
+# NetPBM output, can be converted to png using:
+# dikjstraTest.py | convert /dev/stdin path.png
+img = collections.defaultdict(lambda: (0x0, 0x0, 0x0))
+for i, j in tmp:
+    img[i, j] = (0xFF, 0xFF, 0xFF)
+
+img[start] = (0x00, 0x00, 0xFF)
+img[goal] = (0xFF, 0x00, 0x00)
+
+print('P3')
+print(str(area[2])+' '+str(area[3]))
+print(255)
+for i in range(area[2]):
+    for j in range(area[3]):
+        print(' '.join(str(component) for component in img[i, j]), end=' ')
+    print('')
+
+print(statistics.mean(t), file=sys.stderr)
+print(statistics.stdev(t), file=sys.stderr)
 
