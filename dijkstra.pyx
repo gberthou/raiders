1deaeffe801acb38e3e1948d5c148d5402cfad59
Douglas RAILLARD <public.douglas.raillard@gmail.com>
2017-01-31 01:41:58 +0000

    Clean Dijkstra

--- dijkstra.pyx
+++ dijkstra.pyx
@@ -1,64 +1,78 @@
 # All coordinates are in tile space
-cimport libc.limits
-from libc.stdint cimport uint32_t, int64_t
+#cython: language_level=3
+from libc.stdint cimport uint32_t, uint64_t, UINT64_MAX
 
-def neighborsOf(pos):
-    cdef int x, y
+# Used for any coordinates
+ctypedef uint32_t coord_t
+# Used for distances
+ctypedef uint64_t dist_t
+DIST_MAX = UINT64_MAX
+
+cdef neighborsOf(pos):
+    cdef coord_t x, y
     x, y = pos
     return ((x+1, y), (x-1, y), (x, y+1), (x, y-1))
 
-cdef unsigned int distance(p0, p1):
-    cdef int64_t x0, x1, y0, y1
-    x0,y0 = p0
+cdef dist_t calcDistance(p0, p1):
+    cdef dist_t x0, x1, y0, y1
+    x0, y0 = p0
     x1, y1 = p1
     return (x0 - x1)**2 + (y0 - y1)**2
 
 # x1 > x0
 # y1 > y0
-def dijkstra(area, start, goal):
-    cdef int x0, y0, x1, y1, startX, startY, goalX, goalY
+cpdef dijkstra(area, start, goal):
+    cdef coord_t x0, y0, x1, y1, startX, startY, goalX, goalY
 
     x0, y0, x1, y1 = area
     startX, startY = start
     goalX, goalY   = goal
-    nQ = set()
-    prev = dict()
+    visitedSet     = set()
+    prevMap        = dict()
     
-    dist = {(startX, startY) : 0}
+    distMap = {(startX, startY) : 0}
 
-    cdef int i, j, d
+    cdef dist_t distance, minDistance
+    cdef long i 
     for i in range((x1-x0+1) * (y1-y0+1)):
-        d = libc.limits.INT_MAX
-        for x, j in dist.items():
-            if j < d and (x not in nQ):
-                d = j
-                minDistPoint = x
+        minDistance = DIST_MAX
+        for point, distance in distMap.items():
+            if distance < minDistance and (point not in visitedSet):
+                minDistance = distance
+                minDistPoint = point
 
-        if minDistPoint == goal: # Mathematical proof of correctness?
+        if minDistPoint[0] == goalX and minDistPoint[1] == goalY: # Mathematical proof of correctness?
             break
 
-        nQ.add(minDistPoint)
-        del dist[minDistPoint]
+        visitedSet.add(minDistPoint)
+        del distMap[minDistPoint]
 
         for neighbor in neighborsOf(minDistPoint):
-            if neighbor in nQ:
+            if neighbor in visitedSet:
                 continue
-            alt = d + 1 + distance(neighbor, goal) # A* like
-            if neighbor not in dist.keys() or alt < dist[neighbor]:
-                dist[neighbor] = alt
-                prev[neighbor] = minDistPoint
-    return prev
+            # What does "alt" stands for ?
+            alt = minDistance + 1 + calcDistance(neighbor, goal) # A* like
+            if neighbor not in distMap.keys() or alt < distMap[neighbor]:
+                distMap[neighbor] = alt
+                prevMap[neighbor] = minDistPoint
+    return prevMap
 
 def searchPath(area, start, goal):
     if start == goal:
         return [start]
 
-    prev = dijkstra(area, start, goal)
-    if goal not in prev.keys(): # No path
+    prevMap = dijkstra(area, start, goal)
+    if goal not in prevMap.keys(): # No path
         return None
 
-    ret = [goal]
-    while ret[0] in prev.keys():
-        ret = [prev[ret[0]]] + ret
-    return ret
+    path = [goal]
+    lastPoint = goal
+    while True:
+        try:
+            lastPoint = prevMap[lastPoint]
+        except KeyError:
+            break
+        else:
+            path.append(lastPoint)
+    return reversed(path)
 
