ac7abb73e7eeff0997c7fb323872a07c0787a5d3
gberthou <gautier.berthou@insa-lyon.fr>
2017-05-03 00:06:17 +0200

    Map generation: no water crossing

--- tools/gen_map.py
+++ tools/gen_map.py
@@ -30,6 +30,9 @@ def sgn(x):
         return -1
     return 1
 
+def inWater(n, x, y):
+    return n.at(x, y) < WATER_LEVEL
+
 def generateTiles(n, width, height):
     tiles = [0] * (width * height)
     for y in range(height):
@@ -38,8 +41,7 @@ def generateTiles(n, width, height):
             X = x / width
             Y = y / height
 
-            z = n.at(X, Y)
-            if z < WATER_LEVEL:
+            if inWater(n, X, Y):
                 tiles[index] = 4
             else:
                 dx, dy = n.slopeAt(X, Y)
@@ -53,14 +55,18 @@ def generateTiles(n, width, height):
 def generateDomains(n, width, height):
     MIN_SQUARE_DISTANCE = 25**2
     domains = []
-    for y in range(DOMAIN_SPACE):
-        for x in range(DOMAIN_SPACE):
-            extremum = n.localExtremum(x / DOMAIN_SPACE, y / DOMAIN_SPACE)
-            extremum = (width * extremum[0], height * extremum[1])
-            # Keep extremum only if not too close from the registered ones
-            if sum([noise.d2(extremum, (i[0], i[1])) < MIN_SQUARE_DISTANCE for i in domains]) == 0:
-                faction = random.randrange(FACTION_COUNT)
-                domains.append([extremum[0], extremum[1], faction])
+    coordinates = [(x, y) for y in range(DOMAIN_SPACE) for x in range(DOMAIN_SPACE)]
+    # Shuffle coordinates so that final distribution tends to be uniform
+    random.shuffle(coordinates)
+    for x, y in coordinates:
+        extremum = n.localExtremum(x / DOMAIN_SPACE, y / DOMAIN_SPACE)
+        if inWater(n, extremum[0], extremum[1]):
+            continue
+        extremum = (width * extremum[0], height * extremum[1])
+        # Keep extremum only if not too close from the registered ones
+        if sum([noise.d2(extremum, (i[0], i[1])) < MIN_SQUARE_DISTANCE for i in domains]) == 0:
+            faction = random.randrange(FACTION_COUNT)
+            domains.append([extremum[0], extremum[1], faction])
 
     # Remove random domains if they are too numerous
     while len(domains) > 64:
@@ -89,26 +95,43 @@ def domainGraph(domains):
 
     return graph
 
-def routesFromGraph(tiles, domains, graph, width):
+def routesFromGraph(tiles, n, domains, graph, width, height):
+    # No domain can be located inside a water tile (cf. generateDomains)
     for e in graph:
         p    = (int(domains[e[0]][0]), int(domains[e[0]][1]))
         goal = (int(domains[e[1]][0]), int(domains[e[1]][1]))
+        path = {p}
         while p != goal:
             index = p[0] + p[1] * width
             # Set current tile as road
             tiles[index] = 5
 
-            dx = goal[0] - p[0]
-            dy = goal[1] - p[1]
+            dx = sgn(goal[0] - p[0])
+            dy = sgn(goal[1] - p[1])
+
+            # Do not cross water (bridges yet to come?)
+            if inWater(n, (p[0] + dx) / width, (p[1] + dy) / height):
+                # t = array of (distance to goal, dx, dy)
+                t = []
+                for ax, ay in [(i, j) for i in range(-1, 2) for j in range(-1, 2) if i != 0 or j != 0]:
+                    q = (p[0] + ax, p[1] + ay)
+                    if q in path or inWater(n, q[0] / width, q[1] / height):
+                        continue
+                    t.append((noise.d2(q, goal), ax, ay))
+                # Sort array by distance to goal
+                t.sort()
+                # Take the closest solution to goal
+                dx, dy = t[0][1:] # TODO: Sometimes crashes because t = [] when algorithm comes to a dead end
 
             if dx != 0 and dy != 0:
                 # Set filler road tile so that there is no pure diagonal
                 # (Manhattan distance between two adjacent road tiles should
                 # always be 1)
-                index = p[0] + sgn(dx) + p[1] * width
+                index = p[0] + dx + p[1] * width
                 tiles[index] = 5
 
-            p = (p[0] + sgn(dx) , p[1] + sgn(dy))
+            p = (p[0] + dx , p[1] + dy)
+            path = path.union({p})
 
 def setDomainsAsDebug(tiles, domains, width):
     for d in domains:
@@ -154,7 +177,7 @@ domains = generateDomains(mn, width, height)
 # Generate roads
 print("Generating roads...")
 graph = domainGraph(domains)
-routesFromGraph(tiles, domains, graph, width)
+routesFromGraph(tiles, mn, domains, graph, width, height)
 
 # Debug
 setDomainsAsDebug(tiles, domains, width)
